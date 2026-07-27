793c8c73a436decfa3f02c2a951c34a33b8bb0ee
QuimEsc <150162542+QuimEsc@users.noreply.github.com>
2026-07-27 15:54:31 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.PDC_CONFIG = Object.freeze({
+  appsScriptUrl: "POSA_ACI_LA_URL_D_APPS_SCRIPT",
+  groupId: "PDC3",
+  firebaseRoot: "GamificacioPDC",
+  liveHeartbeatMs: 20000,
+  liveTtlHours: 72
+});
---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+window.firebaseConfig = {
+  apiKey: "POSA_ACI_FIREBASE_API_KEY",
+  authDomain: "POSA_ACI.firebaseapp.com",
+  databaseURL: "https://POSA_ACI-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "POSA_ACI",
+  appId: "POSA_ACI"
+};
